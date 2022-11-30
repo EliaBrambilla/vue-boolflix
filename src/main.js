f3d317ff9240ea0cbe4744a14e1dadfc9d39c46e
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-30 22:20:49 +0100

    aggiorno main js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// IMPORT BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// * import specific icons
+import { faUserSecret, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
+
+// * add icons to the library
+library.add(faUserSecret, faStar, faStarHalfStroke, faStarRegular)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
