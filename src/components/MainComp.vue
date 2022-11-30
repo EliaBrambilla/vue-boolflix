6cc02a24a36d4bb0a4ce436a38f75403e1eb77c2
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-30 22:26:57 +0100

    aggiorno main comp vue

---
+++ src/components/MainComp.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+      <FilmsList :arrayFilm2="arrayFilm" />
+      <SeriesList :arraySerie2="arraySerie" />
+    </main>
+  </template>
+  
+  <script>
+  import FilmsList from './FilmsList.vue'
+  import SeriesList from './SeriesList.vue'
+  export default {
+    name: 'MainComp',
+    components: {
+        FilmsList,
+        SeriesList,
+    },
+    props: {
+      arrayFilm: Array,
+      arraySerie: Array,
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  * {
+    background-color: #424242;
+  }
+  </style>
+  Footer
+  © 2022 GitHub, Inc.
+  Footer navigation
+  Terms
+  Privacy
+  Security
+  Status
+  Doc
